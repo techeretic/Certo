6e866758d2f4665b6cd8f70ef0910b46a40fbaeb
techeretic <techeretic@gmail.com>
2014-04-23 02:06:30 +0530

    Initial Commit

---
+++ src/com/pshetye/Databases/Products.java
@@ -0,0 +1,34 @@
+package com.pshetye.Databases;
+
+/*
+ * Product	
+ * 		Basic product details	•	Product code
+								•	Product name
+								•	Product Category
+								•	Joint life products
+								•	Multiple life products
+								•	Max/Min Age
+								•	Max/Min SA
+								•	NFO options
+								
+		Premium Definition and calculation	
+								•	Region based Premium table definition
+								•	Discounts
+								•	Modal factors
+								
+		Riders/Combination	
+								•	Mandatory and optional Rider definition
+								
+		Event and benefits	
+								•	Death benefits
+								•	Maturity benefits
+								•	Periodic payment/Survival benefit
+								•	Surrender Value
+								•	Surrender charges
+								•	Lien
+
+ * 
+ */
+public class Products {
+
+}
